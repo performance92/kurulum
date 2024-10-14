0162fae838ca8969b29959876c2b414ea990b693
Selim Parti <68921516+performance92@users.noreply.github.com>
2024-10-14 16:47:52 +0300

    Update kurulum.script düzenleme.py

--- kurulum.script düzenleme.py
+++ kurulum.script düzenleme.py
@@ -90,3 +90,114 @@ log "Guacamole bileşenleri kuruluyor..."
 sleep 2
 apt-get update >> "$LOGFILE" 2>&1
 apt-get install -y gcc g++ libcairo2-dev libjpeg-turbo8-dev libpng-dev libtool-bin libossp-uuid-dev libavcodec-dev libavutil-dev libswscale-dev freerdp2-dev libpango1.0-dev libssh․․․
+log "Guacamole bileşenleri kuruluyor..."
+sleep 2
+apt-get update >> "$LOGFILE" 2>&1
+apt-get install -y gcc g++ libcairo2-dev libjpeg-turbo8-dev libpng-dev libtool-bin libossp-uuid-dev libavcodec-dev libavutil-dev libswscale-dev freerdp2-dev libpango1.0-dev libssh2-1-dev libvncserver-dev libtelnet-dev libssl-dev libvorbis-dev libwebp-dev libpulse-dev libavformat-dev ffmpeg cowsay make openjdk-8-jdk tomcat9 tomcat9-admin tomcat9-common tomcat9-user >> "$LOGFILE" 2>&1
+check_error "Guacamole bileşenleri kurulurken hata oluştu."
+log "Guacamole bileşenleri kuruldu."
+sleep 7
+clear
+
+log "Guacamole derleniyor..."
+sleep 2
+cd /home/gardiyan/Gardiyan_Setup/gardiyan_v2_s1/guacamole_setup_new && tar -xf guacamole-server-1.3.0.tar.gz 2>> "$LOGFILE"
+check_error "guacamole-server-1.3.0.tar.gz' dosyası çıkartılamadı."
+cd guacamole-server-1.3.0/
+./configure --with-init-dir=/etc/init.d --disable-dependency-tracking >> "$LOGFILE" 2>&1
+check_error "Guacamole yapılandırması sırasında hata oluştu."
+sleep 2
+make >> "$LOGFILE" 2>&1
+check_error "Make işlemi sırasında hata oluştu."
+sleep 2
+make install >> "$LOGFILE" 2>&1
+check_error "Make install sırasında hata oluştu."
+sleep 2
+ldconfig
+sleep 2
+systemctl stop guacd
+cd /home/gardiyan/Gardiyan_Setup/gardiyan_v2_s1
+clear
+
+log "Openfire kuruluyor..."
+cd /home/gardiyan/Gardiyan_Setup/gardiyan_v2_s1
+sleep 2
+dpkg -i openfire_4.7.1_all.deb >> "$LOGFILE" 2>&1
+check_error "Openfire kurulurken hata oluştu."
+log "Openfire kuruldu."
+sleep 2
+clear
+
+log "SSHPass kuruluyor..."
+sleep 2
+apt-get install sshpass -y >> "$LOGFILE" 2>&1
+check_error "SSHPass kurulurken hata oluştu."
+log "SSHPass kuruldu."
+sleep 2
+clear
+
+systemctl daemon-reload
+chown gardiyan:gardiyan -R /home/gardiyan/Gardiyan
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export PATH=$JAVA_HOME/bin:$PATH
+clear
+
+log "Araçlar kuruluyor..."
+apt-get install -y unzip zip inxi net-tools >> "$LOGFILE" 2>&1
+check_error "Araçlar kurulurken hata oluştu."
+log "Araçlar kuruldu."
+sleep 3
+clear
+
+# Eğer NFS Server kurulumu gerekiyorsa bu bölümü açabiliriz
+# log "NFS Server kuruluyor..."
+# apt-get install nfs-kernel-server -y >> "$LOGFILE" 2>&1
+# check_error "NFS Server kurulurken hata oluştu."
+# log "NFS Server kuruldu."
+# log "NFS Yapılandırma ayarlarını yapınız!!"
+# sleep 5
+# nano exports
+# cp -r exports /etc/
+# sleep 3
+# systemctl daemon-reload
+# systemctl restart nfs-server
+# clear
+
+log "Gardiyan test amaçlı ilk kez çalıştırılıyor..."
+cd /home/gardiyan/Gardiyan/Server/Main/apache-karaf-4.0.10/bin/ && ./start >> "$LOGFILE" 2>&1
+check_error "Gardiyan başlangıcı sırasında hata oluştu."
+sleep 90
+log "Gardiyan M2 uygulandı."
+sleep 2
+clear
+
+log "Sunucunun saat dilimi ayarlanıyor..."
+timedatectl set-timezone Europe/Istanbul >> "$LOGFILE" 2>&1
+check_error "Saat dilimi ayarlanırken hata oluştu."
+log "Saat dilimi ayarlandı."
+cat /etc/timezone
+clear
+
+log "Docker kuruluyor..."
+cd /home/gardiyan/Gardiyan_Setup/gardiyan_v2_s1/docker/
+sudo dpkg -i ./containerd.io_1.6.25-1_amd64.deb \
+./docker-ce_24.0.7-1~ubuntu.20.04~focal_amd64.deb \
+sudo dpkg -i ./containerd.io_1.6.25-1_amd64.deb \
+./docker-ce_24.0.7-1~ubuntu.20.04~focal_amd64.deb \
+./docker-ce-cli_24.0.7-1~ubuntu.20.04~focal_amd64.deb \
+./docker-buildx-plugin_0.11.2-1~ubuntu.20.04~focal_amd64.deb \
+./docker-compose-plugin_2.21.0-1~ubuntu.20.04~focal_amd64.deb >> "$LOGFILE" 2>&1
+check_error "Docker kurulurken hata oluştu."
+log "Docker kuruldu."
+clear
+
+cd /home/gardiyan/DBs
+./import_dbs.sh >> "$LOGFILE" 2>&1
+check_error "Veritabanları içe aktarılırken hata oluştu."
+sleep 10
+
+cd /home/gardiyan/Gardiyan_Setup/gardiyan_v2_s1
+./Setup_2.sh >> "$LOGFILE" 2>&1
+check_error "Setup 2 işlemi sırasında hata oluştu."
+
+cowsay -f tux "Kurulum Tamamlandı. Sunucuyu Yeniden Başlatın..." | tee -a "$LOGFILE"
